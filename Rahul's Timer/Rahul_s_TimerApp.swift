840b5462ffb55795b594627ae331414d91ee403e
rahul <rahulajoshi9@gmail.com>
2024-12-18 17:25:24 -0800

    Initial Commit

---
+++ Rahul's Timer/Rahul_s_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rahul_s_TimerApp.swift
+//  Rahul's Timer
+//
+//  Created by Rahul Joshi on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Rahul_s_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rahul's TimerTests/Rahul_s_TimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Rahul_s_TimerTests.swift
+//  Rahul's TimerTests
+//
+//  Created by Rahul Joshi on 12/18/24.
+//
+
+import Testing
+
+struct Rahul_s_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
